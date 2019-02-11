c81bf3091f50a860f2708c28ade144aedffd4477
dstegner <dmstegner@gmail.com>
2019-02-10 22:18:12 -0500

    added styling

--- src/index.js
+++ src/index.js
@@ -1,24 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+import SeasonDisplay from "./SeasonDisplay";
 
-    // when we want to default numbers we use null
-    this.state = {
-      lat: null,
-      errorMessage: ""
-    };
+class App extends React.Component {
+  // official docs say you shouldn't do data loading in the constructor
+  // because you should centralize where calls are made, and not all
+  // components do not have a constructor (function components)
+  // constructor(props) {
+  //   super(props);
+
+  //   // when we want to default numbers we use null
+  //   this.state = {
+  //     lat: null,
+  //     errorMessage: ""
+  //   };
+  // }
+
+  state = {
+    lat: null,
+    errorMessage: ""
+  };
+
+  // used one time when the page loads
+  // used to load data
+  componentDidMount() {
+    console.log("My component was rendered to the screen");
 
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      err => {
-        this.setState({ errorMessage: err.message });
-      }
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
+  }
+
+  // used every time the page is rerendered
+  // used data loading when data needs to update
+  componentDidUpdate() {
+    console.log("My componet was just updated - it rerendered");
+  }
+
+  // used when the component is no loger shown
+  // good for clean up
+  componentWillUnmount() {
+    console.log("My component was just unmounted");
   }
 
   render() {
@@ -26,7 +50,7 @@ class App extends React.Component {
       return <div>Error: {this.state.errorMessage}</div>;
     }
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
     return <div>Loading!</div>;
