0ae79ff3cbb0808e3a8207295f8a3610de25149d
dstegner <dmstegner@gmail.com>
2019-03-28 21:57:12 -0400

    refactored to make hooks

--- src/index.js
+++ src/index.js
@@ -3,59 +3,22 @@ import ReactDOM from "react-dom";
 
 import SeasonDisplay from "./SeasonDisplay";
 import Spinner from "./Spinner";
+import useLocation from "./useLocation";
 
-class App extends React.Component {
-  // official docs say you shouldn't do data loading in the constructor
-  // because you should centralize where calls are made, and not all
-  // components do not have a constructor (function components)
-  // constructor(props) {
-  //   super(props);
+const App = () => {
+  // empty array means that it will only be called one time
+  const [lat, errorMessage] = useLocation();
 
-  //   // when we want to default numbers we use null
-  //   this.state = {
-  //     lat: null,
-  //     errorMessage: ""
-  //   };
-  // }
-
-  state = {
-    lat: null,
-    errorMessage: ""
-  };
-
-  // used one time when the page loads
-  // used to load data
-  componentDidMount() {
-    console.log("My component was rendered to the screen");
-
-    window.navigator.geolocation.getCurrentPosition(
-      position => this.setState({ lat: position.coords.latitude }),
-      err => this.setState({ errorMessage: err.message })
-    );
+  let content;
+  if (errorMessage) {
+    content = <div>Error: {errorMessage}</div>;
+  } else if (lat) {
+    content = <SeasonDisplay lat={lat} />;
+  } else {
+    content = <Spinner message="Please Accept Location Request" />;
   }
 
-  // used every time the page is rerendered
-  // used data loading when data needs to update
-  componentDidUpdate() {
-    console.log("My componet was just updated - it rerendered");
-  }
-
-  // used when the component is no loger shown
-  // good for clean up
-  componentWillUnmount() {
-    console.log("My component was just unmounted");
-  }
-
-  render() {
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>;
-    }
-    if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />;
-    }
-
-    return <Spinner message="Please Accept Location Request" />;
-  }
-}
+  return <div className="border red">{content}</div>;
+};
 
 ReactDOM.render(<App />, document.querySelector("#root"));
